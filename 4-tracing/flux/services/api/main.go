22e31e9b5288ca3e1fc5f30a1e390dd07d2c2912
Claudson Oliveira <claudsonweb@gmail.com>
2021-12-05 21:25:35 +0000

    Unmarshal request on api

--- 4-tracing/flux/services/api/main.go
+++ 4-tracing/flux/services/api/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+type requestBody struct {
+	Number  int               `json:"number"`
+	Headers map[string]string `json:"headers"`
+}
+
 func sendReceiptHandler(w http.ResponseWriter, req *http.Request) {
 	body, _ := ioutil.ReadAll(req.Body)
+	var request requestBody
+	if err := json.Unmarshal(body, &request); err != nil {
+		log.Printf("failed to unmarshal %s\n", body)
+		return
+	}
 
-	log.Println(string(body))
+	log.Printf("Number=%d TraceID=%s\n", request.Number, request.Headers["x-trace-id"])
 }
 
 func main() {
