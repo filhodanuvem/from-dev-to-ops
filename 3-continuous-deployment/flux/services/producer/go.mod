d96aead69c022218b6c0b7ecdd2747a72b109a87
Claudson Oliveira <claudsonweb@gmail.com>
2021-12-12 23:51:48 +0000

    Use prometheus library instead of opentelemetry

--- 3-continuous-deployment/flux/services/producer/go.mod
+++ 3-continuous-deployment/flux/services/producer/go.mod
@@ -4,19 +4,13 @@ go 1.16
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/nats-io/nats-server/v2 v2.6.5 // indirect
 	github.com/nats-io/nats.go v1.13.1-0.20211018182449-f2416a8b1483
+	github.com/prometheus/client_golang v1.11.0 // indirect
 	github.com/satori/go.uuid v1.2.0
-	go.opentelemetry.io/otel v1.2.0
-	go.opentelemetry.io/otel/exporters/prometheus v0.25.0
-	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.2.0
-	go.opentelemetry.io/otel/metric v0.25.0
-	go.opentelemetry.io/otel/oteltest v0.20.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.2.0
-	go.opentelemetry.io/otel/sdk/export/metric v0.25.0
-	go.opentelemetry.io/otel/sdk/metric v0.25.0
-	go.opentelemetry.io/otel/trace v1.2.0
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- 3-continuous-deployment/flux/services/producer/main.go
+++ 3-continuous-deployment/flux/services/producer/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
@@ -11,17 +9,10 @@ import (
 	"time"
 
 	"github.com/nats-io/nats.go"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	uuid "github.com/satori/go.uuid"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/prometheus"
-	"go.opentelemetry.io/otel/metric/global"
-	"go.opentelemetry.io/otel/sdk/export/metric/aggregation"
-	"go.opentelemetry.io/otel/sdk/metric/aggregator/histogram"
-	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
-	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
-	selector "go.opentelemetry.io/otel/sdk/metric/selector/simple"
-	dtrace "go.opentelemetry.io/otel/trace"
 )
 
 const tracerName = "producer"
@@ -41,6 +32,16 @@ func main() {
 	}
 	defer sc.Close()
 
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":2222", nil)
+	}()
+
+	opsProcessed := promauto.NewCounter(prometheus.CounterOpts{
+		Name: "producer_produced_events_count",
+		Help: "The total number of produced events",
+	})
+
 	js, err := sc.JetStream()
 	if err != nil {
 		log.Fatalf("Couldn't connect to nats %s, err: %s", nats_url, err)
@@ -48,14 +49,9 @@ func main() {
 
 	log.Println("Running crazy producer...")
 
-	initMeter()
-
 	numbers := make(chan int)
 	go func(js nats.JetStreamContext) {
 		for {
-			var span dtrace.Span
-			_, span = otel.Tracer(tracerName).Start(context.Background(), "Produce")
-
 			u1 := uuid.NewV4()
 			number := <-numbers
 			m := message{
@@ -68,20 +64,15 @@ func main() {
 			b, err := json.Marshal(m)
 			if err != nil {
 				log.Printf("Error on publishing to nats: %s\n", err)
-				span.RecordError(err)
-				span.End()
 				continue
 			}
 
 			if _, err := js.Publish(nats_subject, b); err != nil {
 				log.Printf("Error on publishing to nats: %s\n", err)
-				span.RecordError(err)
-				span.End()
 				continue
 			}
 
-			span.SetAttributes(attribute.Int("number", m.Number))
-			span.End()
+			go opsProcessed.Inc()
 			log.Println(m)
 		}
 	}(js)
@@ -91,29 +82,4 @@ func main() {
 		numbers <- number
 		time.Sleep(3 * time.Second)
 	}
-}
-
-func initMeter() {
-	config := prometheus.Config{}
-	c := controller.New(
-		processor.NewFactory(
-			selector.NewWithHistogramDistribution(
-				histogram.WithExplicitBoundaries(config.DefaultHistogramBoundaries),
-			),
-			aggregation.CumulativeTemporalitySelector(),
-			processor.WithMemory(true),
-		),
-	)
-	exporter, err := prometheus.New(config, c)
-	if err != nil {
-		log.Panicf("failed to initialize prometheus exporter %v", err)
-	}
-	global.SetMeterProvider(exporter.MeterProvider())
-
-	http.HandleFunc("/metrics", exporter.ServeHTTP)
-	go func() {
-		_ = http.ListenAndServe(":2222", nil)
-	}()
-
-	fmt.Println("Prometheus server running on :2222")
 }
